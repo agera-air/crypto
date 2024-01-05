60812d3f31b887686f62b95c5e0b2cd72d717efd
hydroper <matheusdiasdesouzads@gmail.com>
2024-01-05 13:04:40 -0300

    Update

--- build.js
+++ build.js
@@ -1,27 +1,38 @@
-const { spawnSync } = require("child_process");
 const { copyFileSync } = require("fs");
+const util = require("util");
+const childProcess_exec = util.promisify(require("child_process").exec);
 const changeDirectory = process.chdir;
 
-changeDirectory("packages/org.agera.crypto.workerShared");
-run("asconfigc");
-copyFileSync("swc/org.agera.crypto.workerShared.swc", "../org.agera.crypto/libs/org.agera.crypto.workerShared.swc");
-copyFileSync("swc/org.agera.crypto.workerShared.swc", "../org.agera.crypto.worker/libs/org.agera.crypto.workerShared.swc");
-changeDirectory("../org.agera.crypto.worker");
-run("asconfigc");
-copyFileSync("build/org.agera.crypto.worker.swf", "../org.agera.crypto/src/org.agera.crypto.worker.swf");
-changeDirectory("../org.agera.crypto");
-run("asconfigc");
-copyFileSync("swc/org.agera.crypto.swc", "tests/libs/org.agera.crypto.swc");
-changeDirectory("tests");
-run("asconfigc");
+class Main {
+    constructor() {
+        this.initialize();
+    }
 
-/**
- * @param {string} object 
- */
-function run(command) {
-    spawnSync(command, {
-        cwd: process.cwd(),
-        detached: true,
-        stdio: "inherit"
-    });
+    async initialize() {
+        changeDirectory("packages/org.agera.crypto.workerShared");
+        await this.run("asconfigc");
+        copyFileSync("swc/org.agera.crypto.workerShared.swc", "../org.agera.crypto/libs/org.agera.crypto.workerShared.swc");
+        copyFileSync("swc/org.agera.crypto.workerShared.swc", "../org.agera.crypto.worker/libs/org.agera.crypto.workerShared.swc");
+        changeDirectory("../org.agera.crypto.worker");
+        await this.run("asconfigc");
+        copyFileSync("build/org.agera.crypto.worker.swf", "../org.agera.crypto/src/org.agera.crypto.worker.swf");
+        changeDirectory("../org.agera.crypto");
+        await this.run("asconfigc");
+        copyFileSync("swc/org.agera.crypto.swc", "tests/libs/org.agera.crypto.swc");
+        changeDirectory("tests");
+        await this.run("asconfigc");
+    }
+
+    /**
+     * @param {string} command
+     */
+    async run(command) {
+        try {
+            const {stdout, stderr} = await childProcess_exec(command);
+            console.log(stdout);
+            console.log(stderr);
+        } catch (error) {}
+    }
 }
+
+new Main();
